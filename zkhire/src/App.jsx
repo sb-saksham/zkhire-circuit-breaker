0bdbb426225431c45b1853f08cc9a3724b0b7483
Saksham Bisen <sakshambisen123@gmail.com>
2024-02-18 07:14:26 +0530

    feat: added contract and noir circuit

--- zkhire/src/App.jsx
+++ zkhire/src/App.jsx
@@ -6,32 +6,30 @@ import LogInPage from "./components/auth/LogInPage";
 import MNavbar from "./components/UI/MNavbar";
 import HomePage from "./components/UI/Homepage";
 import Error404Page from "./components/UI/Error404Page";
+import PostJob from "./components/PostJob";
 import Landing from "./components/codeEditor/Landing";
+import AssessmentTest from "./components/AssessmentTest";
 
 function App() {
     return (
     <BrowserRouter>
         <Routes>
           <Route element={<MNavbar/>} errorElement={<Error404Page/>}>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/coding/test/" element={<Landing/>} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/jobs/">          
-                {/* <Route path=":id/" element={<RequireAuth><IndJobs /></RequireAuth>}>
+                {/* <Route path=":id/" element={<RequireAuth><IndJobs /></RequireAuth>}> */}
                     <Route path="test/">
                         <Route path="coding/" element={<RequireAuth>
-                            <CodingTest/>
+                            <Landing/>
                         </RequireAuth>} />
                         <Route path="quiz/" element={<RequireAuth>
                             <AssessmentTest />
                         </RequireAuth>} />          
-                    </Route> 
+                    {/* </Route>  */}
                 </Route>
                 <Route path="post/" element={<RequireAuth>
-                    <AddJob />
-                </RequireAuth>} /> */}
-                <Route path="apply/" element={
-                    <HomePage />
-                } />
+                    <PostJob />
+                </RequireAuth>} />
                 {/* <Route path="company/" element={<RequireAuth>
                     <CompanyJobs />
                 </RequireAuth>} /> */}
